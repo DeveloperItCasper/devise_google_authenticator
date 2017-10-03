9d28732f69512b013d21c6a4851b364bc420f3a8
Nick <nick@itcasper.com>
2017-10-03 17:50:55 +1300

    change format

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -2,9 +2,8 @@ module DeviseGoogleAuthenticator
   module Controllers # :nodoc:
     module Helpers # :nodoc:
       def google_authenticator_qrcode(user, qualifier=nil, issuer=nil)
-        username = username_from_email(user.email)
         app = user.class.ga_appname || Rails.application.class.parent_name
-        data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}"
+        data = "otpauth://totp/#{otpauth_user(user.email, app, qualifier)}?secret=#{user.gauth_secret}"
         data << "&issuer=#{issuer}" if !issuer.nil?
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
@@ -12,7 +11,7 @@ module DeviseGoogleAuthenticator
       end
 
       def otpauth_user(username, app, qualifier=nil)
-        "#{username}@#{app}#{qualifier}"
+        "#{app}:#{username}"
       end
 
       def username_from_email(email)
